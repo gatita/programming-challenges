42ca9ddc91a213816183f20a985d5273de6a4312
Grace Hatamyar <hatamyarg@gmail.com>
2015-10-18 13:21:57 -0700

    compute bits set to 1

---
+++ bits_set_to_one.py
@@ -0,0 +1,9 @@
+def one_bits(x):
+    """Given an integer, compute the number of bits set to 1"""
+    m = 1
+    count = 0
+    for i in range(x // 2 + 1):
+        if x & m == m:
+            count += 1
+        m = m << 1
+    return count
