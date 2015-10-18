09a91f6b1cce70ed6e056a4c49556b40f0ab4ceb
Grace Hatamyar <hatamyarg@gmail.com>
2015-10-18 13:08:19 -0700

    return new string with vowels removed

---
+++ disemvowel.py
@@ -0,0 +1,10 @@
+def disemvowel(comment):
+    """Code Wars Problem Text:
+    Write a function that takes a string and returns
+    a new string with all vowels removed.
+
+    For example, the string "This website is for losers LOL!" would
+    become "Ths wbst s fr lsrs LL!".
+    """
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    return ''.join([c for c in comment if c.lower() not in vowels])
