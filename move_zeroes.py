f44c80492a0ad0f8284e0718cfcb19e792c4f90c
Grace Hatamyar <hatamyarg@gmail.com>
2015-10-26 16:32:36 -0700

    fix to account for in place deletions

--- move_zeroes.py
+++ move_zeroes.py
@@ -1,6 +1,8 @@
 def move_zeroes(nums):
-    count = nums.count(0)
+    """Given a list, move all 0's to the end while maintaining the
+    order of non-zero elements. Modifies the list in place.
+    """
     for i in nums:
         if i == 0:
             nums.remove(i)
-    nums += count * [0]
+            nums.append(0)
